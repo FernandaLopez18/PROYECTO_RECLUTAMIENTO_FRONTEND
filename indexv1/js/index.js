9cc3df8ed0c66138eb47793ca33c89095463e636
memoorte <74221690+memoorte@users.noreply.github.com>
2022-11-19 21:57:24 -0600

    Add files via upload
    version3

---
+++ indexv1/js/index.js
@@ -0,0 +1,7 @@
+ toastr.warning('Cuando accedes a Outsourcing desde un ordenador, teléfono móvil u otro dispositivo, podemos obtener información de dicho dispositivo sobre tu tipo de navegador, ubicación y dirección IP, así como las páginas que visitas. También utilizamos las cookies para confirmar que estás conectado a Outsourcing, y para saber cuándo estás interactuando con aplicaciones y sitios web de la plataforma Outsourcing. Cuando te conectes a un sitio web o una aplicación de la plataforma, nos suministrarán información, incluida la información acerca de las acciones que realizas.',"políticas de privacidad",{
+            "closeButton": true,
+            "progressBar": false,
+            "positionClass": "toast-bottom-full-width",
+            "showDuration": "3000",
+            "timeOut": "15000",
+        });
